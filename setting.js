084cef1c43449c709b722169220a6f22fbdc8c0c
Shinji De La Kazumi <144513116+ShiinjiZX@users.noreply.github.com>
2025-12-05 14:34:34 +0900

    Upload setting.js

---
+++ setting.js
@@ -0,0 +1,5 @@
+global.autoviewsw = true
+global.swreact = ['👍', '❤️', '🔥', '😂', '😮', '😹', '👏', '🗿','😎','🤪'];
+global.sendTele = false //true = kirim sw ke wa & false = kirim sw ke whatsapp
+global.teletoke = "" //token bot telegram lu
+global.teleid = "" //id akun telegram lu
